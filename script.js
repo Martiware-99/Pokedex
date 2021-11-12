6e9babb62a23efd4865986ae99a3c05589b19da7
Martiware-99 <martin.neyns.pro@gmail.com>
2021-11-12 10:33:14 +0100

    last version

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ let numberOfPokemon = 898
 let container = document.querySelector('.container')
 
 const getPokemon = async id => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemon = await response.json()
-    pokedexEntry(pokemon)
-    console.log(pokemon)
+    const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokemon = await response1.json()    
+    const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
+    const evolution = await response2.json()
+    pokedexEntry(pokemon, evolution)
 }
 
 const fetchPokemons = async () => {
-    for (let index = 1; index <= numberOfPokemon; index++) {
+    for (let index = 1; index <= 30; index++) {
         await getPokemon(index)
     }
 }
 
 fetchPokemons()
 
-const pokedexEntry = pokemon => {
+const pokedexEntry = (pokemon, evolution) => {
     let pokeCard = document.createElement('article')
     pokeCard.classList.add('pokeCard', `${pokemon.types[0].type.name}`)
     container.appendChild(pokeCard)
@@ -37,13 +38,18 @@ const pokedexEntry = pokemon => {
     let pokeName = document.createElement('header')
     pokeName.classList.add('pokeName')
     pokeDescription.appendChild(pokeName)
-
+    
     let moveSet = document.createElement('div')
     moveSet.classList.add('moveSet')
     pokeDescription.appendChild(moveSet)
 
     let devolution = document.createElement('p')
     devolution.classList.add('devolution')
+    if (evolution === null || evolution.evolves_from_species === null){
+        devolution.innerHTML = `${pokemon.name} is the first of it's line`
+    } else if (evolution != null || evolution.evolves_from_species.name != null) {
+        devolution.innerHTML = `${pokemon.name} evolved from ${evolution.evolves_from_species.name}`
+    }
     pokeDescription.appendChild(devolution)
     
     let name = document.createElement('p')
@@ -70,3 +76,4 @@ const pokedexEntry = pokemon => {
         moveSet.appendChild(move)
     }
 }
+
